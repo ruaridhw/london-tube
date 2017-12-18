c659bf7ccb24a474002274086c77b983ef79a4be
Ruaridh Williamson <ruaridh.williamson@icloud.com>
2017-12-19 01:24:46 +1000

    Correct SQL logic

--- 2_analysis/sql/travel_time_calcs.sql
+++ 2_analysis/sql/travel_time_calcs.sql
@@ -1,11 +1,6 @@
--- SELECT JourneyPatternRef, DepartureMins, COUNT(*)
--- FROM VehicleJourneys
--- GROUP BY JourneyPatternRef, DepartureMins
--- HAVING COUNT(*) > 1
-
 -- ---- sql_view_departures ----
 
-CREATE OR REPLACE VIEW departures AS
+CREATE MATERIALIZED VIEW departures AS
 
 WITH arrival_calc AS (
 	SELECT
@@ -13,25 +8,25 @@ WITH arrival_calc AS (
 		 v.*
 		,j."JourneyPatternSections"
 		,j."JourneyPatternTimingLink"
-		
+
 		,j."From_SequenceNumber"
 		,j."From_StopPointRef"
 		,p1."CommonName" "FromStopPointName"
-		
+
 		,j."To_SequenceNumber"
 		,j."To_StopPointRef"
 		,p2."CommonName" "ToStopPointName"
-		
+
 		,j."RunTime"
 		,j."WaitTime"
 		,j."JourneyTime"
-		
+
 		--,arr."ArrivalMins_Link"
 		,v."DepartureMins" + SUM(j."JourneyTime") OVER (PARTITION BY v."VehicleJourneyCode" ORDER BY j."From_SequenceNumber") AS "ArrivalMins_Link"
-		
+
 		,FIRST_VALUE(p1."CommonName") OVER (PARTITION BY v."VehicleJourneyCode"
 		    ORDER BY j."From_SequenceNumber" ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS "OriginStopPointName"
-		
+
 		,LAST_VALUE(p2."CommonName") OVER (PARTITION BY v."VehicleJourneyCode"
 		    ORDER BY j."From_SequenceNumber" ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS "DestStopPointName"
 
@@ -49,10 +44,21 @@ WITH arrival_calc AS (
 	LEFT JOIN "StopPoints" p2
 	ON j."To_StopPointRef" = p2."AtcoCode"
 
-	--,LATERAL (SELECT v."DepartureMins" + SUM(j."JourneyTime") OVER (PARTITION BY v."VehicleJourneyCode" ORDER BY j."FromSequenceNumber") AS "ArrivalMins_Link") arr
+	LEFT JOIN "Services_RegularDayType_DaysOfWeek" d1
+	ON d1."Services" = v."ServiceRef"
 
-	--WHERE v."JourneyPatternRef" = 'JP_1-BAK-_-y05-430200-228-I-16'
-	--WHERE v.DepartureMins BETWEEN 561 AND 563
+	LEFT JOIN "VehicleJourneys_RegularDayType_DaysOfWeek" d2
+	ON d2."VehicleJourneys" = v."VehicleJourneyCode"
+
+	LEFT JOIN "Services" b
+	ON b."ServiceCode" = v."ServiceRef"
+
+	WHERE
+		    d1."DaysOfWeek" IN ('MondayToSaturday', 'MondayToSunday', 'Wednesday')
+		AND d2."DaysOfWeek" IN ('MondayToSaturday', 'MondayToSunday', 'Wednesday')
+
+	AND '2017-12-20' BETWEEN b."OpPeriod_StartDate" AND b."OpPeriod_EndDate"
+
 	ORDER BY v."DepartureMins", j."From_SequenceNumber"
 
 )
@@ -62,9 +68,9 @@ SELECT
 	,LAG("ArrivalMins_Link", 1, "DepartureMins") OVER (PARTITION BY "VehicleJourneyCode" ORDER BY "From_SequenceNumber") AS "DepartureMins_Link"
 FROM arrival_calc
 
--- SELECT 
+-- SELECT
 -- FROM JourneyPatternSections j
 -- ON JourneyPatternSectionID = JourneyPatternSectionRefs
--- 
--- SELECT JourneyPatternID, JourneyPatternSectionRefs	
+--
+-- SELECT JourneyPatternID, JourneyPatternSectionRefs
 -- FROM StandardServices s
