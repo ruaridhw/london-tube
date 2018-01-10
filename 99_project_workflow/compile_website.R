1b81ee1a8c48269cf219c364a962c3889faf9393
Ruaridh Williamson <ruaridh.williamson@icloud.com>
2018-01-10 22:22:41 +0000

    Update website build process

--- 99_project_workflow/compile_website.R
+++ 99_project_workflow/compile_website.R
@@ -1,36 +1,45 @@
 ## ruaridhw.github.io/london-tube build chain
+library(magrittr)
 
 # Setup a clean directory in which to build the site
-build_dir <- "build_site"
+build_dir <- "_build_site"
 if (dir.exists(build_dir)) unlink(build_dir, recursive = TRUE)
 dir.create(build_dir)
 
 # Copy across the template site directory files
-file.copy(from=list.files("4_site", full.names = T), to=build_dir)
+file.copy(from=list.files("4_media", pattern = "(png|mp4)$", full.names = T), to=build_dir)
+file.copy(from=list.files("5_site", full.names = T), to=build_dir)
 
 # Copy all .Rmd files from the 3_notebooks subdirectory
 file.copy(from=list.files("3_notebooks", pattern = ".Rmd$", full.names = TRUE), to=build_dir, 
           overwrite = TRUE)
 
 # Copy specific analysis source files
-source_files <- c("2_analysis/r/GetTablesFromXPaths.R", "2_analysis/r/SQLTables.R",
-                "2_analysis/r/Variable Creation.R",
-                "2_analysis/python/XMLParsing.py")
+source_files <- c(
+  "2_analysis/r/GetTablesFromXPaths.R",
+  "2_analysis/r/SQLTables.R",
+  "2_analysis/r/VariableCreation.R",
+  
+  "2_analysis/python/XMLParsing.py",
+  "2_analysis/python/TfLTimetable.py",
+  "2_analysis/python/ShortestPath.py",
+  
+  "2_analysis/sql/DaysOfWeekGroups.sql",
+  "2_analysis/sql/DepartureBoard.sql",
+  "2_analysis/sql/InboundGraph.sql")
 file.copy(from=source_files, to=build_dir, overwrite = TRUE)
 
 # Apply knitr::spin to the analysis files to compile them to .Rmd reports
-spin_files <- list.files(build_dir, pattern = "\\.(R|py)$", full.names = TRUE)
+spin_files <- list.files(build_dir, pattern = "\\.(R|py|sql)$", full.names = TRUE)
 spin_output <- lapply(spin_files, knitr::spin, format = "Rmd", knit = FALSE) %>% unlist
 
-# Build a new environment with `eval=FALSE` prespecified to avoid compiling the
+# Set `eval=FALSE` temporarily to avoid compiling the
 # code chunks in the source files beyond plain markdown
-no_eval <- new.env()
-with(no_eval,
-     knitr::opts_chunk$set(eval = FALSE)
-)
-lapply(spin_output, rmarkdown::render, output_format = "github_document", envir = no_eval)
+knitr::opts_chunk$set(eval = FALSE, python.reticulate = FALSE)
+lapply(spin_output, rmarkdown::render, output_format = "github_document")
 # Remove Rmd and html preview for source files
 file.remove(c(spin_output,spin_files, gsub("Rmd", "html", spin_output)))
+knitr::opts_chunk$set(eval = TRUE)
 
 # Render the ManagingData notebook to a GitHub html preview
 rmarkdown::render(list.files(build_dir, pattern = "ManagingData", full.names = T),
