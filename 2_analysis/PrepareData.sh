dd453de0aa79dfdfc9cd2b7978b0e8205eb14613
Ruaridh Williamson <ruaridh.williamson@icloud.com>
2018-01-12 17:26:55 +0000

    Update site

--- 2_analysis/PrepareData.sh
+++ 2_analysis/PrepareData.sh
@@ -11,6 +11,7 @@
 #' XMLParsing.py contains the locations of the input and output data directories
 #' It parses the raw XML to a number of Pandas dataframes and dumps a directory
 #' of Feather files for interpretation by R
+#' For usage see `python3 python/XMLParsing.py --help`
 python3 python/XMLParsing.py ../1_data/1_1_raw_data/timetables/data ../1_data/1_2_processed_data
 
 #' FeatherUpload.R reads the Feather files and appends them to existing tables
